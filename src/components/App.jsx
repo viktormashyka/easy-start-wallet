8699299ac736fa312f331a718defc4c96a7f8a7b
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2023-02-17 21:22:25 +0000

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,11 @@ export const App = () => {
         color: '#010101',
       }}
     >
+<<<<<<< HEAD
       React Kapu$ta template - 7
+=======
+      React homework template
+>>>>>>> parent of b32bb2e (Update App.jsx)
     </div>
   );
 };
