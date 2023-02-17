e2c7e71e90e84a9e754267a68b5ae35012969fcc
Ruslan379 <arnov@i.ua>
2023-02-18 01:21:34 +0200

    add files

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,47 +2,40 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import {
     persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+    // persistReducer,
+    // FLUSH,
+    // REHYDRATE,
+    // PAUSE,
+    // PERSIST,
+    // PURGE,
+    // REGISTER,
 } from 'redux-persist';
 
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
 import { authReducer } from 'redux/auth/authSlice';
-import { transactionsReducer } from 'redux/transaction/transactionSlice';
-
-
+// import { transactionsReducer } from 'redux/transaction/transactionSlice';
 
 //! Persisting token field from auth slice to localstorage
-const authPersistConfig = {
-    key: 'auth',
-    storage,
-    whitelist: ['token'],
-};
-
-
+// const authPersistConfig = {
+//     key: 'auth',
+//     storage,
+//     whitelist: ['token'],
+// };
 
 export const store = configureStore({
     reducer: {
-        auth: persistReducer(authPersistConfig, authReducer),
-        transactions: transactionsReducer,
+        // auth: persistReducer(authPersistConfig, authReducer),
+        auth: authReducer,
+        // transactions: transactionsReducer,
     },
-    middleware: (getDefaultMiddleware) => [
-        ...getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }),
-    ],
+    //   middleware: getDefaultMiddleware => [
+    //     ...getDefaultMiddleware({
+    //       serializableCheck: {
+    //         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    //       },
+    //     }),
+    //   ],
     devTools: process.env.NODE_ENV === 'development',
 });
 
 export const persistor = persistStore(store);
-
-
-
-
