5f5806e28eb9686d15706863deceee74e5765bef
Iuliia Dolnikova <dolnikova80@gmail.com>
2023-02-18 20:38:36 +0200

    add_login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,63 @@
 // import {} from './LoginForm.styled';
+// import { Component } from 'react';
+
+import { Formik, Form, ErrorMessage } from 'formik';
+import {
+  Button,
+  ButtonGoogl,
+  Container,
+  Div,
+  Input,
+  P,
+  ErrorText,
+} from './LoginForm.styled';
+import * as yup from 'yup';
+
+const schema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Enter your email'),
+  password: Yup.string().min(8).max(20).required('Enter your password'),
+});
+const FormError = ({ name }) => {
+  return (
+    <ErrorMessage
+      name={name}
+      render={message => <ErrorText>{message}</ErrorText>}
+    />
+  );
+};
 
 export const LoginForm = () => {
-  return <div>Login form on Login page</div>;
+  const handleSubmit = (velues, { resetForm }) => {
+    console.log(velues);
+    resetForm();
+  };
+  return (
+    <Container>
+      <P>You can log in with your Google Account:</P>
+      <ButtonGoogl type="button">Google</ButtonGoogl>
+      <P>Or log in using an email and password, after registering:</P>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        <Form autoComplete="off">
+          <label htmlFor="login">
+            Email:
+            <Input type="text" name="email" placeholder="your email" />
+            <FormError name="email" component="div" />
+            {/* <br /> */}
+          </label>
+          <label htmlFor="password">
+            Password:
+            <Input type="password" name="password" />
+            <FormError name="password" />
+          </label>
+          <Div>
+            <Button type="submit">LOG IN</Button>
+          </Div>
+        </Form>
+      </Formik>
+    </Container>
+  );
 };
--- src/components/LoginForm/LoginForm.styled.js
+++ src/components/LoginForm/LoginForm.styled.js
@@ -1,1 +1,91 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import { Field } from 'formik';
+
+export const Container = styled.div`
+  margin: auto;
+  margin-top: 117px;
+  max-height: 552px;
+  max-width: 426px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 12px;
+  color: #010101;
+  flex-direction: column;
+  border: 2px solid white;
+  border-radius: 30px;
+  box-shadow: 0px 10px 60px rgba(170, 178, 197, 0.2);
+  padding: 56px 83px 56px 83px;
+  font-family: 'Roboto';
+  font-style: normal;
+`;
+
+export const Input = styled(Field)`
+font-size: 22px;
+  margin-bottom: 32px;
+  margin-top: 5px;
+  display: flex;
+  padding-left: 19.54px;
+  // flex-direction: column;
+  width: 258.92px;
+  height: 52px;
+  border-radius: 30px;
+  background: #f6f7fb;
+  border-style: none;
+  border: none;
+   &: hover,  &:focus {
+    border:  non;
+  }
+`;
+export const ButtonGoogl = styled.button`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  width: 119.2px;
+  height: 40px;
+  background: #f6f7fb;
+  border-radius: 26px;
+  border-style: none;
+  letter-spacing: 0.02em;
+  cursor: pointer;
+  border: none;
+`;
+export const P = styled.p`
+  // margin-left: auto;
+  // margin-right: auto;
+  font-size: 12px;
+  line-height: 14px;
+  max-width: 210px;
+  max-height: 28px;
+  color: #52555f;
+  // display: flex;
+  // justify-content: center;
+`;
+export const Button = styled.button`
+  width: 122.17px;
+  height: 44px;
+  background: #f5f6fb;
+  border-radius: 16px;
+  border-style: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #52555f;
+  font-weight: 700;
+  cursor: pointer;
+  &: hover,  &:focus {
+    background: #ff751d;
+    color: #ffffff;
+    // border-style: none;
+    border: none;
+  } ;
+`;
+export const Div = styled.div`
+  display: flex;
+  margin-top: '40px';
+  justify-content: space-between;
+`;
+export const ErrorText = styled.p`
+  color: red;
+`;
