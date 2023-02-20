c06d5956c9e80bee983656aa9a2535128186038b
Kateryna Kleshch <ketrins1993@gmail.com>
2023-02-20 16:12:35 +0200

    Update authOperations.js

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -230,10 +230,9 @@ export const updateBalance = createAsyncThunk(
     async (credentials, thunkAPI) => {
         console.log("auth/changeBalance --> credentials:", credentials); //!
         try {
-            // const { data } = await axios.patch('/users/balance', credentials);
-            // console.log("auth/changeBalance == >data.balance:", data.balance); //!
-            // return data.balance;
-            return credentials;
+            const { data } = await axios.patch('/users/balance', credentials);
+            console.log("auth/changeBalance == >data.balance:", data.balance); //!
+            return data.balance;
         } catch (error) {
             console.log(error); //!
             toast.error(`Ошибка запроса: ${error.message === "Request failed with status code 404" ? "Нет такой коллекции пользователей" : error.message}`, { position: "top-center", autoClose: 2000 });
