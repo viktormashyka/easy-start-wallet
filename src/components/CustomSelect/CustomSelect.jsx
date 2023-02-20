3d20aa6a23b348aa1a61666e8fcbfb9dd3c90caa
Yuliya Zozulya <djulia@ukr.net>
2023-02-20 21:27:19 +0200

    fixing logisc, styles

--- src/components/CustomSelect/CustomSelect.jsx
+++ src/components/CustomSelect/CustomSelect.jsx
@@ -1,7 +1,10 @@
 import Select from 'react-select';
 
-export function CustomSelect({ onChange, options, value, type }) {
+export function CustomSelect({ onChange, options, value, type, isLoading }) {
   const defaultValue = (options, value) => {
+    if (value === '') {
+      return true;
+    }
     return options ? options.find(option => option.value === value) : '';
   };
 
@@ -14,6 +17,7 @@ export function CustomSelect({ onChange, options, value, type }) {
         onChange={value => onChange(value)}
         options={options}
         placeholder={type}
+        isLoading={isLoading}
         required
       />
     </>
--- src/components/TopButtonWrapper/TopBtnWrapper.styled.js
+++ src/components/TopButtonWrapper/TopBtnWrapper.styled.js
@@ -30,8 +30,10 @@ export const ButtonLink = styled.a`
   text-transform: uppercase;
   line-height: 1.17;
   letter-spacing: 2%;
-  transition: color 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
+  transition: color 250ms cubic-bezier(0.075, 0.82, 0.165, 1),
+    background-color 250ms cubic-bezier(0.075, 0.82, 0.165, 1);
   &:hover {
     color: #ff751d;
+    background-color: #fefefe;
   }
 `;
