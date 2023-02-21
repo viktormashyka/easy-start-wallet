c691a4255b43d0d309090689ed1cef95372a426f
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2023-02-21 18:18:45 +0200

    start

---
+++ src/hooks/useScreenResizing.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+function getScreenResizing() {
+  const { clientWidth: width, clientHeight: height } = document.documentElement;
+  return {
+    width,
+    height,
+  };
+}
+
+export default function useScreenResizing() {
+  const [screenResizing, setScreenResizing] = useState(getScreenResizing());
+
+  useEffect(() => {
+    function handleResize() {
+      setScreenResizing(getScreenResizing());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return screenResizing;
+}
