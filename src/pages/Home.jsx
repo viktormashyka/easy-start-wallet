45004b01b3daf47025bad39c3a4f130de122bc09
Yuliya Zozulya <djulia@ukr.net>
2023-02-21 02:54:12 +0200

    adding btns

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
         <title>Home page</title>
       </Helmet>
       <>This is home page</>
-      <BalanceWrapper />
+      {/* <BalanceWrapper /> */}
       <MainHome>
         <ExpensesForm />
         {/* <IncomeForm /> */}
