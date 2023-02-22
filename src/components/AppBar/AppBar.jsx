e1ff8a6004b0ac8d9ddb4c6472480211156c6257
Yuliya Zozulya <djulia@ukr.net>
2023-02-23 00:16:29 +0200

    fixing comments

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,15 +1,15 @@
-import { Navigation } from 'components/Navigation/Navigation';
+// import { Navigation } from 'components/Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
 import { AuthNav } from '../AuthNav/AuthNav';
 import { useAuth } from 'hooks';
 import { AppBarForm } from './AppBar.styled';
-import { Header } from 'components/Header/Header';
+// import { Header } from 'components/Header/Header';
 
 // for test
-import { OperationList } from 'components/OperationList/OperationList';
+// import { OperationList } from 'components/OperationList/OperationList';
 import { IncomeForm } from 'components/IncomeForm/IncomeForm';
 import { MainHome } from 'components/MainHome/MainHome';
-import { ExpensesForm } from 'components/ExpensesForm/ExpensesForm';
+// import { ExpensesForm } from 'components/ExpensesForm/ExpensesForm';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
