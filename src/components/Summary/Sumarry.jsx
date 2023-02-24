3647beec82167813fab57d9170dc7077316d3dbe
Demon1056 <102851685+Demon1056@users.noreply.github.com>
2023-02-24 23:29:14 +0200

    add logic Summary

--- src/components/Summary/Sumarry.jsx
+++ src/components/Summary/Sumarry.jsx
@@ -8,18 +8,34 @@ import {
 } from './Summary.styled';
 import { useSelector } from 'react-redux';
 import { selectAllTransactions } from '../../redux/transaction/transactionSelectors.js';
+import moment from 'moment';
 
 export const Summary = () => {
-  let transactions = useSelector(selectAllTransactions);
-  if (transactions.length < 1) {
+  const transactions = useSelector(selectAllTransactions);
+  const transactionsTypeValue = 'income';
+  // transactionsTypeValue - у майбутньому пропс який буде приходити в залежності від локейшн
+  const typeArray = transactions.filter(
+    ({ transactionsType }) => transactionsType === transactionsTypeValue
+  );
+  let result = typeArray.reduce((accumulator, current) => {
+    const existing = accumulator.find(item => item.date === current.date);
+    if (existing) {
+      existing.sum += current.sum;
+    } else {
+      accumulator.push({ ...current });
+    }
+    return accumulator;
+  }, []);
+
+  if (result.length < 1) {
     return (
       <SummaryFalse>
         <SummaryTitle>SUMMARY</SummaryTitle>
       </SummaryFalse>
     );
   }
-  if (transactions.length > 6) {
-    transactions = transactions.slice(0, 6);
+  if (result.length > 6) {
+    result = transactions.slice(0, 6);
   }
   return (
     <div>
@@ -30,9 +46,9 @@ export const Summary = () => {
           </Tr>
         </thead>
         <tbody>
-          {transactions.map(({ _id, date, sum }) => (
+          {result.map(({ _id, date, sum }) => (
             <Tr key={_id}>
-              <Td>{date}</Td>
+              <Td>{moment(date).format('MMMM')}</Td>
               <Td>{sum}</Td>
             </Tr>
           ))}
