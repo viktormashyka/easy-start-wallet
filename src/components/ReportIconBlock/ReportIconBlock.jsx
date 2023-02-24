27eaef32dd0829bb43afd9f3ab20360687a6f9a8
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2023-02-24 16:27:31 +0200

    fix component

--- src/components/ReportIconBlock/ReportIconBlock.jsx
+++ src/components/ReportIconBlock/ReportIconBlock.jsx
@@ -7,6 +7,7 @@ import expenseIconCategories from './data/expenseIcon.json';
 import incomeIconCategories from './data/incomeIcon.json';
 
 import {
+  Container,
   ReportWrapper,
   TransactionWrapper,
   ArrowСhangeMonth,
@@ -18,91 +19,106 @@ import {
 } from './ReportIconBlock.styled';
 
 export const ReportIconBlock = () => {
+  // eslint-disable-next-line no-unused-vars
   const [category, setCategory] = useState('');
-  const [type, setType] = useState('expense');
+  const [type, setType] = useState('expenses');
 
-  console.log('category', category);
-  // тут  мені потрібен селектор який дає інфу по транзакціям за місяць ⬇️⬇️⬇️⬇️
-  const transaction = useSelector(selectAllTransactionsReport);
-  // ⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️⬆️
-
-  // отримую відфільтровані транзакції по типу (витрати чи доходи)
-  //   const getTransactionByType = type => {
-  //     const filteredByType = transaction.filter(
-  //       transaction => transaction.type === type
-  //     );
-  //     return filteredByType;
-  //   };
-  // отримую загальну суму по категорії (витрат або доходів)
-  //   const findTotalSumByCategory = (type, category) => {
-  //     let totalExpense = 0;
-  //     getTransactionByType(type)
-  //       .filter(tr => tr.category === category)
-  //       .map(el => (totalExpense += el.sum));
-  //     return totalExpense;
-  //   };
-
-  const onHandleChangeType = () => {
-    if (type === 'expense') {
-      setType('income');
-      setCategory('');
-    }
-    if (type === 'income') {
-      setType('expense');
-      setCategory('');
-    }
-  };
+  const categories =
+    type === 'expenses' ? expenseIconCategories : incomeIconCategories;
   const getCategory = e => {
     setCategory(e.target.attributes.title.nodeValue);
   };
 
-  const categories =
-    type === 'expense' ? expenseIconCategories : incomeIconCategories;
+  const report = useSelector(selectAllTransactionsReport);
 
+  const transaction = report.filterTransactions;
+
+  const getTransactionByType = type => {
+    if (transaction) {
+      const filteredByType = transaction.filter(
+        transaction => transaction.transactionsType === type
+      );
+      console.log('filteredByType', filteredByType);
+      return filteredByType;
+    }
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const categoryLabel = categories.map(el => el.label);
+
+  const findTotalSumByCategory = (type, categoryLabel) => {
+    let totalExpense = 0;
+    getTransactionByType(type)
+      .filter(tr => tr.category === categoryLabel)
+      .map(el => (totalExpense += el.sum));
+    return totalExpense;
+  };
+
+  const onHandleChangeType = () => {
+    if (type === 'expenses') {
+      setType('income');
+      setCategory('');
+    }
+    if (type === 'income') {
+      setType('expenses');
+      setCategory('');
+    }
+  };
+
+  //   const SortTrBySum = (type, categoryLabel) => {
+  //     return getTransactionByType(type)
+  //       .filter(tr => tr.category === categoryLabel)
+  //       .sort((first, second) => second.sum - first.sum);
+  //   };
+
+  //   console.log('SortTrBySum', SortTrBySum());
+  //   //   const label = categories.map(cat => cat.label);
+  //   const id = categories.map(cat => cat.id);
   return (
-    <ReportWrapper>
-      <TransactionWrapper>
-        <ArrowСhangeMonth viewBox="0 0 7 12" onClick={onHandleChangeType}>
-          <path d="M6 1L2 6L6 11" stroke="#FF751D" />
-        </ArrowСhangeMonth>
-        {type === 'expense' ? (
-          <ReportTitle>Expenses</ReportTitle>
-        ) : (
-          <ReportTitle>Income</ReportTitle>
-        )}
-        <ArrowСhangeMonth viewBox="0 0 7 12" onClick={onHandleChangeType}>
-          <path d="M1 1L5 6L1 11" stroke="#FF751D" width="4" height="10" />
-        </ArrowСhangeMonth>
-      </TransactionWrapper>
-      <ReportList>
-        {/* Тут треба перевіряти чи масив транзакцій не дорівнює 0⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️*/}
-        {transaction.balance === 0 ? (
-          <p>
-            Отчет будет доступен после того как вы внесете данные о своих
-            доходах и расходах за выбранный период.
-          </p>
-        ) : (
-          categories.map(event => {
-            // let sum = findTotalSumByCategory(type, event.label);
-            // if (sum === 0) {
-            //   return null;
-            // }
+    <Container>
+      <ReportWrapper>
+        <TransactionWrapper>
+          <ArrowСhangeMonth viewBox="0 0 7 12" onClick={onHandleChangeType}>
+            <path d="M6 1L2 6L6 11" stroke="#FF751D" />
+          </ArrowСhangeMonth>
+          {type === 'expenses' ? (
+            <ReportTitle>Expenses</ReportTitle>
+          ) : (
+            <ReportTitle>Income</ReportTitle>
+          )}
+          <ArrowСhangeMonth viewBox="0 0 7 12" onClick={onHandleChangeType}>
+            <path d="M1 1L5 6L1 11" stroke="#FF751D" width="4" height="10" />
+          </ArrowСhangeMonth>
+        </TransactionWrapper>
+        <ReportList>
+          {!getTransactionByType(type) ? (
+            <p>
+              Отчет будет доступен после того как вы внесете данные о своих
+              доходах и расходах за выбранный период.
+            </p>
+          ) : (
+            categories.map(event => {
+              let sum = findTotalSumByCategory(type, event.label);
+              if (sum === 0) {
+                return null;
+              }
 
-            return (
-              <ReportCard key={event.id}>
-                <p>100.00</p>
-                <IconSvg title={event.label} onClick={getCategory}>
-                  <use
-                    xlinkHref={`${sprite}#${event.label}`}
-                    title={event.label}
-                  />
-                </IconSvg>
-                <ReportCardTitle>test</ReportCardTitle>
-              </ReportCard>
-            );
-          })
-        )}
-      </ReportList>
-    </ReportWrapper>
+              return (
+                <ReportCard key={event.id}>
+                  <p>{`${sum.toLocaleString('ru')}.00`}</p>
+                  <IconSvg title={event.label} onClick={getCategory}>
+                    <use
+                      xlinkHref={`${sprite}#${event.label}`}
+                      title={event.label}
+                    />
+                  </IconSvg>
+                  <ReportCardTitle>{event.label}</ReportCardTitle>
+                </ReportCard>
+              );
+            })
+          )}
+        </ReportList>
+      </ReportWrapper>
+    </Container>
   );
 };
