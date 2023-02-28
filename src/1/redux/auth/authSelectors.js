f26de0fb5519bff12c8914a72446b5df7c79867a
Viktor Mashyka <99292051+viktormashyka@users.noreply.github.com>
2023-02-28 18:50:02 +0000

    delete report style jsx

--- src/1/redux/auth/authSelectors.js
+++
@@ -1,9 +0,0 @@
-export const selectUser = state => state.auth.user;
-
-export const selectIsLoggedIn = state => state.auth.isLoggedIn;
-
-export const selectIsRegistrIn = state => state.auth.isRegistrIn;
-
-export const selectIsRefreshing = state => state.auth.isRefreshing;
-
-export const selectBalance = state => state.auth.balance;
--- src/1/redux/transaction/transactionSelectors.js
+++
@@ -1,5 +0,0 @@
-
-export const selectLoadingTransactions = state => state.transactions.isLoading;
-
-
-export const selectAllTransactions = state => state.transactions.allTransactions;
