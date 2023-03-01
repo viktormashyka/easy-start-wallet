ee1a165d8ba961228e73fbc0af0f13afb0174ecd
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2023-03-01 17:39:55 +0200

    Update OperationList.jsx
    Прибрала Table з OperationList

--- src/components/OperationList/OperationList.jsx
+++ src/components/OperationList/OperationList.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import useScreenResizing from '../../hooks/useScreenResizing';
 import * as SC from './OperationList.styles';
 import { ReactComponent as DeleteIcon } from '../../images/delete.svg';
-import { Table } from 'components/Table/Table';
-import { TableStyle } from '../Table/Table.styled';
 import { deleteTransaction } from '../../redux/transaction/transactionOperations';
 import { useDispatch } from 'react-redux';
 import moment from 'moment';
@@ -12,10 +9,9 @@ import { useState } from 'react';
 const modalQuestion = 'Are you sure?';
 // Mikhaylo Pobochikh
 
-export const OperationList = ({ sortedTransactions, isLoading }) => {
+export const OperationList = ({ sortedTransactions }) => {
   const [showModal, setShowModal] = useState(false);
   const [selectedId, setSelectedId] = useState(null);
-  const viewPort = useScreenResizing();
   const dispatch = useDispatch();
 
   const onHandleClick = () => {
@@ -27,43 +23,15 @@ export const OperationList = ({ sortedTransactions, isLoading }) => {
     setShowModal(true);
   };
 
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: 'Date',
-        accessor: 'date', // accessor is the "key" in the data
-      },
-      {
-        Header: 'Description',
-        accessor: 'description',
-      },
-      {
-        Header: 'Category',
-        accessor: 'category',
-      },
-      {
-        Header: 'Sum',
-        accessor: 'sum',
-      },
-      {
-        Header: '',
-        accessor: 'icon',
-      },
-    ],
-    []
-  );
-
   return (
     <>
+      <SC.List >
       {sortedTransactions.map(el => {
         const expenses = el.transactionsType === 'expenses';
         const newSum = `${Number(el.sum).toFixed(2).padStart(5, 0)}UAN.`
         // console.log('el._id', el._id);
         return (
-          // Від 320 px до 768
-
-          <SC.List key={el._id}>
-            <SC.OperationListWrapper>
+            <SC.OperationListWrapper key={el._id}>
               <SC.OperationListDiv>
                 <SC.OperationListTitle>{el.description}</SC.OperationListTitle>
                 <SC.OperationListDivDate>
@@ -81,7 +49,6 @@ export const OperationList = ({ sortedTransactions, isLoading }) => {
                 ) : (
                   <SC.OperationListDivBalanceText>{` ${newSum}`}</SC.OperationListDivBalanceText>
                 )}
-
                 <SC.DeleteBtn
                   type="button"
                   id={el._id}
@@ -91,21 +58,9 @@ export const OperationList = ({ sortedTransactions, isLoading }) => {
                 </SC.DeleteBtn>
               </SC.OperationListDivBalance>
             </SC.OperationListWrapper>
-          </SC.List>
         );
       })}
-      {/* Для 769 px + */}
-
-      {viewPort.width > 767 && !isLoading && (
-        <TableStyle TableStyle>
-          <Table
-            columns={columns}
-            data={sortedTransactions}
-            onHandleClick={clickButton}
-          />
-        </TableStyle>
-      )}
-
+        </SC.List>
       {showModal && (
         <UniversalModal
           closeModal={setShowModal}
