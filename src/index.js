f5bda13b276229fa2e5727988eadd61247eb9cb0
viktormashyka <99292051+viktormashyka@users.noreply.github.com>
2023-12-20 20:34:06 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import { store, persistor } from './redux/store';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+// import { store } from './redux/store';
 import 'modern-normalize';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
@@ -12,12 +12,12 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}> */}
-    {/* <BrowserRouter basename="/easy-start-wallet"> */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-    {/* </PersistGate> */}
+    <PersistGate loading={null} persistor={persistor}>
+      {/* <BrowserRouter basename="/easy-start-wallet"> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
   // </React.StrictMode>
 );
